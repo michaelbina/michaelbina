798bd0b4cd3a16b050b634e366bb1fed09528e7a
Michael Bina <michaelbina@icloud.com>
2018-12-04 13:22:02 -0800

    remove geocode

--- js/googlemap_init.js
+++ js/googlemap_init.js
@@ -19,14 +19,14 @@ function googlemap_init(dom_obj, address, description, point) {
 		disableDefaultUI: false,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var custom_map = new google.maps.Geocoder();
-	custom_map.geocode( { "address": address}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			googlemap_init_obj.address = results[0].geometry.location;
-			googlemap_create();
-		} else
-			alert("Geocode was not successful for the following reason: " + status);
-	});
+	// var custom_map = new google.maps.Geocoder();
+	// custom_map.geocode( { "address": address}, function(results, status) {
+	// 	if (status == google.maps.GeocoderStatus.OK) {
+	// 		googlemap_init_obj.address = results[0].geometry.location;
+	// 		googlemap_create();
+	// 	} else
+	// 		alert("Geocode was not successful for the following reason: " + status);
+	// });
 	
 	jQuery(window).resize(function() {
 		if (googlemap_init_obj.map) googlemap_init_obj.map.setCenter(googlemap_init_obj.address_position);
